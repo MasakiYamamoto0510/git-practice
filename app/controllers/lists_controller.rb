6ba37ad8f10cbc46cf0c1495658d5b0f718cf5b3
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2024-12-19 05:06:16 +0000

    [Finish] アプリを完成させよう　第７章

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,7 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_parames)
+    list = List.new(list_params)
     list.save
     redirect_to list_path(list.id)
   end
@@ -18,10 +18,23 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   private
-  def list_parames
-    params.require(:list).permit(:title, :body)
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
   end
 end
